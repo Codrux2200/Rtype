aed53feebeee24f751667d615fd387a4b7c0f2b2
Saad Berrada <72012880+Codrux2200@users.noreply.github.com>
2023-09-19 14:24:01 +0200

    [ADD] | CMakeList.txt install.sh install.bash make installeur for windows and linux

---
+++ build/CMakeFiles/MySFMLProject.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MySFMLProject.dir/main.cpp.o"
+  "CMakeFiles/MySFMLProject.dir/main.cpp.o.d"
+  "MySFMLProject"
+  "MySFMLProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MySFMLProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
