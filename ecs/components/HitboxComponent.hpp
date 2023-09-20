6cb3bab6e696f80e3cb8531712c6093976d72110
chris <christophe.chhor@epitech.eu>
2023-09-20 10:02:00 +0200

    MOD ecs/components/HitboxComponent.hpp Add the HitboxComponent class to ECS namespace, and rename the class respecting camelcase

--- ecs/components/HitboxComponent.hpp
+++ ecs/components/HitboxComponent.hpp
@@ -9,38 +9,47 @@
 #define HITBOXCOMPONENT_HPP_
 #include "Acomponent.hpp"
 
-class hitboxComponent : public ECS::Acomponent {
-    public:
-        /**
-         * @brief Construct a new hitbox Component object
-         * 
-         * @param x 
-         * @param y
-         * @param uid
-         */
-        hitboxComponent(int x, int y, int uid);
-        /**
-         * @brief Destroy the hitbox Component object
-         * 
-         */
-        ~hitboxComponent();
-        /**
-         * @brief Get the X hitbox object
-         * 
-         * @return int
-         */
-        int getX() const;
-        /**
-         * @brief Get the Y hitbox object
-         * 
-         * @return int
-         */
-        int getY() const;
-    protected:
-    private:
-        int x;
-        int y;
-};
-
+namespace ECS {
+    class HitboxComponent : public ECS::Acomponent {
+        public:
+            /**
+             * @brief Construct a new hitbox Component object
+             * 
+             * @param x 
+             * @param y
+             * @param uid
+             */
+            HitboxComponent(int x, int y, int uid);
+            /**
+             * @brief Destroy the hitbox Component object
+             * 
+             */
+            ~HitboxComponent();
+            /**
+             * @brief Get the X hitbox object
+             * 
+             * @return int
+             */
+            int getX() const;
+            /**
+             * @brief Get the Y hitbox object
+             * 
+             * @return int
+             */
+            int getY() const;
+        protected:
+        private:
+            /**
+             * @brief define the x hitbox of the entity
+             * 
+             */
+            int x;
+            /**
+             * @brief define the y hitbox of the entity
+             * 
+             */
+            int y;
+    };
+}
 
 #endif /* !HITBOXCOMPONENT_HPP_ */
