e97e0a5948102a52924482941f4bcca993f8042c
chris <christophe.chhor@epitech.eu>
2023-09-20 10:02:29 +0200

    MOD ecs/components/PlayerComponent.hpp Add the PlayerComponent class to ECS namespace, and rename the class respecting camelcase

--- ecs/components/PlayerComponent.hpp
+++ ecs/components/PlayerComponent.hpp
@@ -9,19 +9,19 @@
 #define PLAYERCOMPONENT_HPP_
 #include "Acomponent.hpp"
 
-class playerComponent : public ECS::Acomponent {
+class PlayerComponent : public ECS::Acomponent {
     public:
         /**
          * @brief Construct a new player Component object
          * 
          * @param id
          */
-        playerComponent(int uid);
+        PlayerComponent(int uid);
         /**
          * @brief Destroy the player Component object
          * 
          */
-        ~playerComponent();
+        ~PlayerComponent();
     protected:
     private:
 };
