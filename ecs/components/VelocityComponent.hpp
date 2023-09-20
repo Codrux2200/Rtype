218c8abab2a4f7be2f68d66bbc90e44428058342
chris <christophe.chhor@epitech.eu>
2023-09-20 10:13:50 +0200

    MOD ecs/components/VelocityComponent.hpp Add the VelocityComponent class to ECS namespace, and rename the class respecting camelcase

--- ecs/components/VelocityComponent.hpp
+++ ecs/components/VelocityComponent.hpp
@@ -9,34 +9,36 @@
 #define VELOCITYCOMPONENT_HPP_
 #include "Acomponent.hpp"
 
-class velocityComponent : public ECS::Acomponent {
-    public:
-        /**
-         * @brief Construct a new velocity Component object
-         * 
-         * @param uid 
-         */
-        velocityComponent(int uid);
-        /**
-         * @brief Destroy the velocity Component object
-         * 
-         */
-        ~velocityComponent();
-        /**
-         * @brief Set the velocity of the entity
-         * 
-         * @param velocity
-         */
-        void setVelocity(int velocity);
-        /**
-         * @brief Get the velocity of the entity
-         * 
-         * @return int
-         */
-        int getVelocity() const;        
-    protected:
-    private:
-        int velocity;
-};
+namespace ECS {
+    class VelocityComponent : public ECS::Acomponent {
+        public:
+            /**
+             * @brief Construct a new velocity Component object
+             * 
+             * @param uid 
+             */
+            VelocityComponent(int uid);
+            /**
+             * @brief Destroy the velocity Component object
+             * 
+             */
+            ~VelocityComponent();
+            /**
+             * @brief Set the velocity of the entity
+             * 
+             * @param velocity
+             */
+            void setVelocity(int velocity);
+            /**
+             * @brief Get the velocity of the entity
+             * 
+             * @return int
+             */
+            int getVelocity() const;        
+        protected:
+        private:
+            int velocity;
+    };
+}
 
 #endif /* !VELOCITYCOMPONENT_HPP_ */
