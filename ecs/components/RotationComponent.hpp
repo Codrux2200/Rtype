a9b68acbe8c082df6583532204cf003806e80044
chris <christophe.chhor@epitech.eu>
2023-09-20 10:05:13 +0200

    MOD ecs/components/RotationComponent.hpp Add the RotationComponent class to ECS namespace, and rename the class respecting camelcase

--- ecs/components/RotationComponent.hpp
+++ ecs/components/RotationComponent.hpp
@@ -8,38 +8,39 @@
 #ifndef ROTATIONCOMPONENT_HPP_
 #define ROTATIONCOMPONENT_HPP_
 #include "Acomponent.hpp"
-
-class rotationComponent : public ECS::Acomponent {
-    public:
-        /**
-         * @brief Construct a new rotation Component object
-         * 
-         * @param uid 
-         */
-        rotationComponent(int uid);
-        /**
-         * @brief Destroy the rotation Component object
-         * 
-         */
-        ~rotationComponent();
-        /**
-         * @brief Get the Rotation object
-         * 
-         */
-        int getRotation() const;
-        /**
-         * @brief Set the Rotation object
-         * 
-         * @param rotation 
-         */
-        void setRotation(float rotation);
-    protected:
-    private:
-        /**
-         * @brief define the rotation of the entity
-         * 
-         */
-        float rotation;
-};
+namespace ECS {
+    class RotationComponent : public ECS::Acomponent {
+        public:
+            /**
+             * @brief Construct a new rotation Component object
+             * 
+             * @param uid 
+             */
+            RotationComponent(int uid);
+            /**
+             * @brief Destroy the rotation Component object
+             * 
+             */
+            ~RotationComponent();
+            /**
+             * @brief Get the Rotation object
+             * 
+             */
+            int getRotation() const;
+            /**
+             * @brief Set the Rotation object
+             * 
+             * @param rotation 
+             */
+            void setRotation(float rotation);
+        protected:
+        private:
+            /**
+             * @brief define the rotation of the entity
+             * 
+             */
+            float rotation;
+    };
+}
 
 #endif /* !ROTATIONCOMPONENT_HPP_ */
