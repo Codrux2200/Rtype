a033f8f54ce116280e31fb7b638a18c201485938
chris <christophe.chhor@epitech.eu>
2023-09-20 10:12:29 +0200

    MOD ecs/components/ShootComponent.hpp Add the ShootComponent class to ECS namespace, and rename the class respecting camelcase

--- ecs/components/ShootComponent.hpp
+++ ecs/components/ShootComponent.hpp
@@ -9,55 +9,57 @@
 #define SHOOTCOMPONENT_HPP_
 #include "Acomponent.hpp"
 
-class shootComponent : public ECS::Acomponent {
-    public:
-        /**
-         * @brief Construct a new shoot Component object
-         * 
-         * @param uid 
-         */
-        shootComponent(int uid, int shootSpeed, int shootDamage);
-        /**
-         * @brief Destroy the shoot Component object
-         * 
-         */
-        ~shootComponent();
-        /**
-         * @brief Get the Shoot Speed object
-         * 
-         * @return int 
-         */
-        int getShootSpeed() const;
-        /**
-         * @brief Get the Shoot Damage object
-         * 
-         * @return int 
-         */
-        int getShootDamage() const;
-        /**
-         * @brief Set the Shoot Speed object
-         * 
-         * @param shootSpeed 
-         */
-        void setShootSpeed(int shootSpeed);
-        /**
-         * @brief Set the Shoot Damage object
-         * 
-         * @param shootDamage 
-         */
-        void setShootDamage(int shootDamage);
-    protected:
-    private:
-        /**
-         * @brief define the shoot speed of the entity
-         * 
-         */
-        int shootSpeed;
-        /**
-         * @brief define the shoot damage of the entity
-         * 
-         */
-        int shootDamage;
-};
+namespace ECS {
+    class ShootComponent : public ECS::Acomponent {
+        public:
+            /**
+             * @brief Construct a new shoot Component object
+             * 
+             * @param uid 
+             */
+            ShootComponent(int uid, int shootSpeed, int shootDamage);
+            /**
+             * @brief Destroy the shoot Component object
+             * 
+             */
+            ~ShootComponent();
+            /**
+             * @brief Get the Shoot Speed object
+             * 
+             * @return int 
+             */
+            int getShootSpeed() const;
+            /**
+             * @brief Get the Shoot Damage object
+             * 
+             * @return int 
+             */
+            int getShootDamage() const;
+            /**
+             * @brief Set the Shoot Speed object
+             * 
+             * @param shootSpeed 
+             */
+            void setShootSpeed(int shootSpeed);
+            /**
+             * @brief Set the Shoot Damage object
+             * 
+             * @param shootDamage 
+             */
+            void setShootDamage(int shootDamage);
+        protected:
+        private:
+            /**
+             * @brief define the shoot speed of the entity
+             * 
+             */
+            int shootSpeed;
+            /**
+             * @brief define the shoot damage of the entity
+             * 
+             */
+            int shootDamage;
+    };
+}
 
 #endif /* !SHOOTCOMPONENT_HPP_ */
