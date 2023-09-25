9898d19673dbc2e127e063a0cb7e9d872bec4e19
chris <christophe.chhor@epitech.eu>
2023-09-25 15:10:22 +0200

    ADD ecs/Entity.cpp Add the Entity class

---
+++ ecs/Entity.cpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2023
+** Rtype
+** File description:
+** Entity
+*/
+
+#include "Entity.hpp"
+
+ECS::Entity::Entity(int id)
+{
+    _id = id;
+    components = std::vector<IComponent>();
+    _tags = std::vector<Tag>();
+}
+
+ECS::Entity::~Entity()
+{
+    components.clear();
+    _tags.clear();
+    components.~vector<IComponent>();
+    _tags.~vector<Tag>();
+}
