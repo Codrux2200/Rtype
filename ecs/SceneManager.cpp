472afef05bf4431e78947377a759ded95d14256e
chris <christophe.chhor@epitech.eu>
2023-09-25 15:10:41 +0200

    ADD ecs/SceneManager.cpp Add the SceneManager class

---
+++ ecs/SceneManager.cpp
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2023
+** Rtype
+** File description:
+** SceneManager
+*/
+
+#include "SceneManager.hpp"
+
+ECS::SceneManager::SceneManager()
+{
+    _scenes = std::map<SceneType, Scene>();
+    _currentScene = SceneType::MAIN_MENU;
+    _shouldClose = false;
+}
+
+ECS::SceneManager::~SceneManager()
+{
+    _scenes.clear();
+    _scenes.~map<SceneType, Scene>();
+}
