253c9078f7e416ef84b88ca459388dd4b1662d45
Saad Berrada <72012880+Codrux2200@users.noreply.github.com>
2023-09-26 11:06:27 +0200

    [FIX] | delete vcpkg and install with cmake file

--- install.sh
+++ install.sh
@@ -1,25 +1,12 @@
-if ! [ -d "vcpkg" ]; then
-  git clone https://github.com/microsoft/vcpkg.git
-  cd vcpkg
-  ./bootstrap-vcpkg.sh
-  ./vcpkg integrate install
-  cd ..
-fi
-
 if ! [ -x "$(command -v cmake)" ]; then
   echo "CMake n'est pas installé, installation en cours..."
-  wget https://github.com/Kitware/CMake/releases/download/v3.22.0/cmake-3.22.0-Linux-x86_64.sh
+  curl -l https://github.com/Kitware/CMake/releases/download/v3.22.0/cmake-3.22.0-Linux-x86_64.sh
   chmod +x cmake-3.22.0-Linux-x86_64.sh
   ./cmake-3.22.0-Linux-x86_64.sh --skip-license --prefix=/usr/local
   rm cmake-3.22.0-Linux-x86_64.sh
   echo "CMake est maintenant installé."
 fi
 
-./vcpkg/vcpkg install sfml
-./vcpkg/vcpkg install asio
 
-mkdir -p build
-cd build
-
-cmake -DCMAKE_TOOLCHAIN_FILE="../vcpkg/scripts/buildsystems/vcpkg.cmake" ..
+cmake . .
 cmake --build .
