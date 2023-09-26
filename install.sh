5fe3f4c13e33886a40554750226e757e3e62a6e7
Saad Berrada <72012880+Codrux2200@users.noreply.github.com>
2023-09-26 11:48:12 +0200

    [FIX] | install.sh install cmake with the local package manager

--- install.sh
+++ install.sh
@@ -1,12 +1,41 @@
-if ! [ -x "$(command -v cmake)" ]; then
-  echo "CMake n'est pas installé, installation en cours..."
-  curl -l https://github.com/Kitware/CMake/releases/download/v3.22.0/cmake-3.22.0-Linux-x86_64.sh
-  chmod +x cmake-3.22.0-Linux-x86_64.sh
-  ./cmake-3.22.0-Linux-x86_64.sh --skip-license --prefix=/usr/local
-  rm cmake-3.22.0-Linux-x86_64.sh
+#!/bin/bash
+
+detect_package_manager() {
+  if [ -x "$(command -v apt-get)" ]; then
+    package_manager="apt-get"
+  elif [ -x "$(command -v yum)" ]; then
+    package_manager="yum"
+  elif [ -x "$(command -v brew)" ]; then
+    package_manager="brew"
+  else
+    package_manager="none"
+  fi
+  echo "$package_manager"
+}
+package_manager=$(detect_package_manager)
+
+if [ "$package_manager" == "apt-get" ]; then
+  echo "APT détecté, installation de CMake..."
+  sudo apt-get update
+  sudo apt-get install cmake
   echo "CMake est maintenant installé."
+elif [ "$package_manager" == "yum" ]; then
+  echo "YUM détecté, installation de CMake..."
+  sudo yum install cmake
+  echo "CMake est maintenant installé."
+elif [ "$package_manager" == "brew" ]; then
+  echo "Homebrew détecté, installation de CMake..."
+  brew install cmake
+  echo "CMake est maintenant installé."
+else
+  echo "Aucun gestionnaire de paquets pris en charge n'a été détecté. Veuillez installer CMake manuellement."
+  exit 1
 fi
+
 
 
 cmake . .
 cmake --build .
+
+sudo rm -fr _deps
+
