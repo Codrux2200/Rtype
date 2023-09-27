210e38f2e1b6adb4b3bce18073a945c7aabee9ec
Christophe CHHOR <christophechhor@christophes-imac.home>
2023-09-27 11:18:26 +0200

    MOD ecs/Core.cpp Update the mainloop

--- ecs/Core.cpp
+++ ecs/Core.cpp
@@ -6,6 +6,8 @@
 */
 
 #include "Core.hpp"
+#include "PlayerComponent.hpp"
+#include "HealthComponent.hpp"
 
 ECS::Core::Core()
 {
@@ -24,10 +26,25 @@ void ECS::Core::mainLoop()
     while(!sceneManager.shouldClose) {
         if (arrow == 10)
             sceneManager.shouldClose = true;
-        arrow += 1;
-        Scene firstScene = sceneManager.getScene(SceneType::MAIN_MENU);
-        firstScene.entitiesList.insert(std::pair<int, Entity>(arrow, Entity(arrow)));
-        sceneManager.getScene(SceneType::MAIN_MENU).loadEntity(arrow);
-        sceneManager.getScene(SceneType::MAIN_MENU).unloadEntity(arrow);
+        Scene firstScene = sceneManager.getScene(ECS::SceneType::MAIN_MENU);
+        // create the player entity if it doesn't exist
+        if (firstScene.entitiesList.size() < 1) {
+            // create a tag vector
+            std::vector<Tag> tags;
+            tags.push_back(ECS::Tag::MOVABLE);
+            tags.push_back(ECS::Tag::DESTROYABLE);
+            firstScene.entitiesList.insert(std::pair<int, Entity>(1, Entity(1, tags)));
+            // create the player component
+            firstScene.entitiesList[1].components.push_back(PlayerComponent(10));
+            // create the health component
+            firstScene.entitiesList[1].components.push_back(HealthComponent(11));
+        } else {
+            // update the health component
+            firstScene.entitiesList[1].components[1].setHealth(100 - arrow * 10);
+        }
+        // close the window if the health is 0
+        if (firstScene.entitiesList[1].components[1].getHealth() == 0)
+            sceneManager.shouldClose = true;
+        arrow++;
     }
 }
