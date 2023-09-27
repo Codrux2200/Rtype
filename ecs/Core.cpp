ab8f6f6761601d09a04ccd72257eba0b3b9105e0
Christophe CHHOR <christophe.chhor@epitech.eu>
2023-09-27 17:58:53 +0200

    MOD ecs/Core.cpp Update the mainloop, try to insert a new entity

--- ecs/Core.cpp
+++ ecs/Core.cpp
@@ -23,6 +23,7 @@ void ECS::Core::mainLoop()
 {
     int arrow = 0;
 
+    sceneManager.setScene(ECS::SceneType::MAIN_MENU, Scene(ECS::SceneType::MAIN_MENU));
     while(!sceneManager.shouldClose) {
         if (arrow == 10)
             sceneManager.shouldClose = true;
@@ -33,18 +34,25 @@ void ECS::Core::mainLoop()
             std::vector<Tag> tags;
             tags.push_back(ECS::Tag::MOVABLE);
             tags.push_back(ECS::Tag::DESTROYABLE);
-            firstScene.entitiesList.insert(std::pair<int, Entity>(1, Entity(1, tags)));
+            // create the player entity
+            Entity player = Entity(1, tags);
+            // it's crashing here and I don't know why
+            firstScene.entitiesList.insert(std::pair<int, Entity>(1, player));
             // create the player component
-            firstScene.entitiesList[1].components.push_back(PlayerComponent(10));
+            firstScene.entitiesList.at(1).components.push_back(PlayerComponent(10));
             // create the health component
-            firstScene.entitiesList[1].components.push_back(HealthComponent(11));
+            firstScene.entitiesList.at(1).components.push_back(HealthComponent(11));
         } else {
-            // update the health component
-            firstScene.entitiesList[1].components[1].setHealth(100 - arrow * 10);
+            // update the health component casted to HealthComponent
+            HealthComponent *health = dynamic_cast<HealthComponent *>(&firstScene.entitiesList.at(1).components[1]);
+            health->setHealth(health->getHealth() - arrow * 10);
+            health->~HealthComponent();
         }
         // close the window if the health is 0
-        if (firstScene.entitiesList[1].components[1].getHealth() == 0)
+        HealthComponent *health = dynamic_cast<HealthComponent *>(&firstScene.entitiesList.at(1).components[1]);
+        if (health->getHealth() <= 0)
             sceneManager.shouldClose = true;
+        health->~HealthComponent();
         arrow++;
     }
 }
