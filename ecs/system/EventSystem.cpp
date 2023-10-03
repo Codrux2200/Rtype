664aa99096429ce643f4b4a3f3531fd1386d26ed
Christophe CHHOR <christophe.chhor@epitech.eu>
2023-10-03 12:27:06 +0200

    [mod] adding movement

--- ecs/system/EventSystem.cpp
+++ ecs/system/EventSystem.cpp
@@ -6,11 +6,42 @@
 */
 
 #include "EventSystem.hpp"
+#include "PositionComponent.hpp"
 
 ECS::EventSystem::EventSystem()
+    : ASystem(ECS::SystemType::EVENT)
 {
 }
 
 ECS::EventSystem::~EventSystem()
 {
+}
+
+static void movePlayer(ECS::Entity *player)
+{
+    std::vector<int> position;
+
+    ECS::PositionComponent *positionComponent = dynamic_cast<ECS::PositionComponent *>(player->components.at(2));
+    position = positionComponent->getValue();
+    if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Z) || sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::W)) 
+    && position.at(1) > 0)
+        player->components.at(2)->setValue(position.at(0), position.at(1) - 1);
+    if ((sf::Keyboard::isKeyPressed(sf::Keyboard::S) || sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+    && position.at(1) < 720)
+        player->components.at(2)->setValue(position.at(0), position.at(1) + 1);
+    if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Q) || sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+    && position.at(0) > 0)
+        player->components.at(2)->setValue(position.at(0) - 1, position.at(1));
+    if ((sf::Keyboard::isKeyPressed(sf::Keyboard::D) || sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+    && position.at(0) < 1280)
+        player->components.at(2)->setValue(position.at(0) + 1, position.at(1));
+}
+
+void ECS::EventSystem::update(SceneManager &sceneManager, SceneType SceneType, int deltaTime)
+{
+    ECS::Scene &actualScene = sceneManager.getScene(SceneType);
+    ECS::Entity *player = actualScene.entitiesList.at(0);
+    std::vector<int> position;
+    // update with a movement event
+    movePlayer(player);    
 }
--- ecs/system/EventSystem.hpp
+++ ecs/system/EventSystem.hpp
@@ -8,12 +8,26 @@
 #ifndef PARTICLESYSTEM_HPP_
 #define PARTICLESYSTEM_HPP_
 #include "ASystem.hpp"
+#include <SFML/Graphics.hpp>
 
 namespace ECS {
     /**
      * @brief Event System
      * 
      */
+    enum SFMLKeys {
+        up = sf::Keyboard::Up,
+        down = sf::Keyboard::Down,
+        left = sf::Keyboard::Left,
+        right = sf::Keyboard::Right,
+        space = sf::Keyboard::Space,
+        w = sf::Keyboard::W,
+        a = sf::Keyboard::A,
+        z = sf::Keyboard::Z,
+        q = sf::Keyboard::Q,
+        s = sf::Keyboard::S,
+        d = sf::Keyboard::D,
+    };
     class EventSystem : public ASystem{
         public:
             /**
@@ -25,7 +39,15 @@ namespace ECS {
              * @brief Destroy the Event System object
              * 
              */
-            ~EventSystem();      
+            ~EventSystem();
+            /**
+             * @brief update the event system
+             * 
+             * @param sceneManager 
+             * @param SceneType 
+             * @param deltaTime 
+             */
+            void update(SceneManager &sceneManager, SceneType SceneType, int deltaTime) final;
         protected:
         private:
     };
