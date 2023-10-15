8c6e672d4cca80fce6b45dc4d778c641a654ed97
christophechr <christophe.chhor@epitech.eu>
2023-10-15 20:33:58 +0200

    "MOD EnemyComponent move the files to the client folder"

--- client/ecs/components/EnemyComponent.hpp
+++ client/ecs/components/EnemyComponent.hpp
@@ -7,41 +7,47 @@
 
 #ifndef ENEMYCOMPONENT_HPP_
 #define ENEMYCOMPONENT_HPP_
+
 #include <string>
-#include "AComponent.hpp"
+#include <memory>
+#include "EventComponent.hpp"
 
 namespace ECS {
     /**
      * @brief Enemy component
      *
      */
-    class EnemyComponent : public ECS::AComponent {
+    class EnemyComponent : public ECS::EventComponent {
         public:
             /**
              * @brief Construct a new enemy Component object
              *
              * @param uid
              */
-            EnemyComponent();
+            EnemyComponent(eventCallback callback);
             /**
              * @brief Destroy the enemy Component object
              *
              */
-            ~EnemyComponent();
-            /**
-             * @brief Get the enemy of the entity
-             *
-             * @return std::vector<int>
-             */
-            void update(const std::vector<int> positions);
+            ~EnemyComponent() = default;
             /**
              * @brief clone the component
              *
              * @return std::shared_ptr<IComponent>
              */
             std::shared_ptr<IComponent> clone() const override;
+            /**
+             * @brief execute the component
+             *
+             * @param packetManager
+             * @param packetsQueue
+             * @param entity
+             * @param dt
+             */
+            void execute(Network::PacketManager &packetManager, std::vector<Network::Packet> &packetsQueue, ECS::Entity &entity, float dt) final override;
         protected:
         private:
+            int _speed = 100;
     };
 }
 
