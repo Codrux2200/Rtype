ab8bf4bdf70a56d46b6d863e666f49e87aebe0b7
Saad Berrada <72012880+Codrux2200@users.noreply.github.com>
2023-10-15 12:22:21 +0200

    [FIX] change install.bat

--- install.bat
+++ install.bat
@@ -1,32 +1,42 @@
 @echo off
-:: Installation de Chocolatey
-if not exist "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" goto install_powershell
-choco -?
-if %errorlevel% neq 0 goto install_chocolatey
 
-choco install -y cmake
+choco -? >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Chocolatey n'est pas installé. Installation en cours...
+    @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
+    if %errorlevel% neq 0 goto install_failed
+) else (
+    echo Chocolatey est déjà installé.
+)
+
+cmake --version >nul 2>&1
+if %errorlevel% neq 0 (
+    echo CMake n'est pas installé. Installation en cours...
+    choco install -y cmake
+    if %errorlevel% neq 0 goto install_failed
+) else (
+    echo CMake est déjà installé.
+)
+
+git --version >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Git n'est pas installé. Installation en cours...
+    choco install -y git
+    if %errorlevel% neq 0 goto install_failed
+) else (
+    echo Git est déjà installé.
+)
+
+cmake .
+
 if %errorlevel% neq 0 goto install_failed
 
-choco install -y git
-if %errorlevel% neq 0 goto install_failed
-
-cmake --version
-if %errorlevel% neq 0 goto install_failed
-cmake <votre_commande_cmake>  :: Remplacez <votre_commande_cmake> par la commande CMake que vous souhaitez exécuter
+echo Configuration de CMake terminée avec succès.
 
 goto end
 
-:install_powershell
-echo PowerShell n'est pas installé. Veuillez l'installer et réessayer.
-exit /b 1
-
-:install_chocolatey
-echo Chocolatey n'est pas installé. Installation en cours...
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
-if %errorlevel% neq 0 goto install_failed
-
 :install_failed
-echo Une erreur s'est produite lors de l'installation. Vérifiez les erreurs ci-dessus.
+echo Une erreur s'est produite lors de l'installation ou de la configuration. Vérifiez les erreurs ci-dessus.
 exit /b 1
 
 :end
