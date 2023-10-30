75dfa99fef831880a996bb55a5fcc79e91902329
Christophe CHHOR <91553820+christophechr@users.noreply.github.com>
2023-10-30 14:15:09 +0100

    MOD install.bat modify the installer to only install packages

--- install.bat
+++ install.bat
@@ -1,52 +1,38 @@
 @echo off
 
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo The script must be launched as administrator.
+    exit /b 1
+)
+
 for /f %%a in ('wmic cpu get NumberOfCores ^| findstr /r "[0-9]"') do (
     set "cores=%%a"
 )
 
-choco -? >nul 2>&1
-if %errorlevel% neq 0 (
-    echo Chocolatey n'est pas installé. Installation en cours...
-    @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
-    if %errorlevel% neq 0 goto install_failed
-) else (
-    echo Chocolatey est déjà installé.
-)
-
 cmake --version >nul 2>&1
 if %errorlevel% neq 0 (
-    echo CMake n'est pas installé. Installation en cours...
-    choco install -y cmake
-    if %errorlevel% neq 0 goto install_failed
+    echo CMake not installed, installing...
+    winget install -e --id Kitware.CMake
 ) else (
-    echo CMake est déjà installé.
+    echo CMake already installed.
 )
 
 git --version >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Git n'est pas installé. Installation en cours...
-    choco install -y git
-    if %errorlevel% neq 0 goto install_failed
+    echo Git not installed, installing...
+    winget install -e --id Git.Git
 ) else (
-    echo Git est déjà installé.
+    echo Git already installed.
 )
 
-cmake -B build
-cmake --build build --config Release -j %cores%
-
-move build\Release\rtype-server.exe .
-move build\Release\rtype-client.exe .
-
-if %errorlevel% neq 0 goto install_failed
-
-echo %errorlevel%
-
-echo Configuration de CMake terminée avec succès.
+exit
+powershell -Command "Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser -Force"
 
 goto end
 
 :install_failed
-echo Une erreur s'est produite lors de l'installation ou de la configuration. Vérifiez les erreurs ci-dessus.
+echo Install failed, check the logs for more information.
 exit /b 1
 
 :end
